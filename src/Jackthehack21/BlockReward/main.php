7f09d51ae5ee7497b1e9f53af41aa1c31e03c7a1
Jackthehaxk21 <gangnam253@gmail.com>
2018-06-09 17:58:39 +0100

    Initial, big

--- src/Jackthehack21/BlockReward/main.php
+++ src/Jackthehack21/BlockReward/main.php
@@ -1,31 +1,61 @@
 <?php
 
-namespace Jackthehack21/BlockReward;
+namespace Jackthehack21\BlockReward;
 
-use pocketmine\command\ConsoleCommandSender;
-use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat as C;
 use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as C;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+
 
 class main extends PluginBase implements Listener
 {
     public function onEnable()
     {
+        if (!is_dir($this->getDataFolder())) {
+            @mkdir($this->getDataFolder());
+        }
+
+        $this->saveResource("config.yml");
+        $this->cfg = new Config($this->getDataFolder()."config.yml", Config::YAML, []);
+        if($this->cfg->get('debug')){
+            $this->getLogger()->info('[BlockReward] - Plugin enabled !');
+        }
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         
     }
 
     public function onDisable()
     {
-        
+        if($this->cfg->get('debug')){
+            $this->getLogger()->info('[BlockReward] - Plugin Disabled !');
+        }
     }
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
     {
         if ($cmd->getName() == 'blockreward') {
-            
+            if(isset($args[0])){
+                switch(strtolower($args[0])){
+                    case 'help':
+                        $sender->sendMessage(C::GOLD.'==== '.C::AQUA.'Help'.C::GOLD.' ====');
+                        $sender->sendMessage(C::GREEN.'/blockreward help '.C::BLACK.'- '.C::GOLD.'Show the help page.');
+                        $sender->sendMessage(C::GREEN.'/blockreward credits '.C::BLACK.'- '.C::GOLD.'Who made me, find out here.');
+                        break;
+                    default:
+                        $sender->sendMessage(C::RED.'Not a valid arg, try /blockreward help');
+                        break;
+                }
+            } else {
+                $sender->sendMessage(C::RED.'Not a valid arg, try /blockreward help');
+                return true;
+            }
         }
         return true;
     }
